3f0d91bea813798a3886bf14ac71c1175f1664a7
aidanohara1 <aohara1@bu.edu>
2023-08-16 16:09:56 -0400

    big ups

---
+++ CP3_assignment_2.R
@@ -0,0 +1,33 @@
+# YOUR NAME HERE #
+
+# CP3 Assignment 2
+
+# Matrices
+
+# Exercise 1
+
+#sadMatrix <- 
+  
+#largeMatrix <-
+
+#smallMatrix <- 
+
+# Exercise 2
+
+i <- 1:8
+j <- 1:8
+
+#square <- 
+
+# Arrays
+
+# Exercise 3
+
+# thisArray <- 
+
+# Exercise 4
+
+# showMatrix <- 
+# matrixTranspose <- 
+# solveMatrix <- 
+
